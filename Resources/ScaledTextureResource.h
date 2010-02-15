6e6f8e549ab12ef1ecedfb40c340131d709c25bc
asgerhoedt <asgerhoedt@gmail.com>
2010-02-15 15:33:38 +0000

    Made a generel templated texture and can be templated to any type

--- Resources/ScaledTextureResource.h
+++ Resources/ScaledTextureResource.h
@@ -10,7 +10,8 @@
 #ifndef _SCALED_TEXTURE_RESOURCE_H_
 #define _SCALED_TEXTURE_RESOURCE_H_
 
-#include <Resources/ITextureResource.h>
+#include <Resources/Texture2D.h>
+#include <Logging/Logger.h>
 
 #include <boost/serialization/base_object.hpp>
 #include <boost/serialization/export.hpp>
@@ -26,9 +27,9 @@ using namespace std;
  *
  * @class ScaledTextureResource ScaledTextureResource.h Resources/ScaledTextureResource.h
  */
-class ScaledTextureResource : public ITextureResource {
+template <class T> class ScaledTextureResource : public Texture2D<T> {
 private:
-    ITextureResourcePtr originalResource;
+    Texture2DPtr(T) originalResource;
     unsigned int scale;
 
 public:
@@ -39,21 +40,67 @@ public:
      * @param resource the texture to scale.
      * @param scale number of pixels to skip.
      */
-    ScaledTextureResource(ITextureResourcePtr resource, unsigned int scale)
-        : ITextureResource(), 
+    ScaledTextureResource(Texture2DPtr(T) resource, unsigned int scale)
+        : Texture2D<T>(), 
           originalResource(resource), scale(scale) {}
 
     //friend class boost::serialization::access;
     template<class Archive>
     void serialize(Archive& ar, const unsigned int version) {
-        ar & boost::serialization::base_object<ITextureResource>(*this);
+        ar & boost::serialization::base_object<ITexture2D>(*this);
     }
 
-    ~ScaledTextureResource();
+    ~ScaledTextureResource(){
+        Unload();
+    }
 
     // resource methods
-    void Load();
-    void Unload();
+    void Load() {
+        if (this->data) return;
+        originalResource->Load();
+    
+        unsigned int scalex = scale, scaley = scale;
+    
+        // @todo: test that width, height % scale != 0 works
+        if (originalResource->GetWidth() % scalex != 0 ||
+            originalResource->GetHeight() % scaley != 0) {
+            logger.warning << "scaled texture cannot be divided with scaling factor";
+            logger.warning << logger.end;
+        }
+
+        this->channels = originalResource->GetChannels();
+        this->width = originalResource->GetWidth() / scalex;
+        this->height = originalResource->GetHeight() / scaley;
+        this->format = originalResource->GetColorFormat();
+        unsigned int size = this->width * this->height * this->channels;
+    
+        T* data = new T[size];
+
+        T* originalData = originalResource->GetData();
+
+        for (unsigned int y=0, j=0; y<this->height;
+             y++, j+=scaley) {
+            
+            for (unsigned int x=0,i=0; x<this->width*this->channels;
+                 x+=this->channels,i+=scalex*this->channels) {
+
+                for(unsigned int k=0; k<this->channels; k++) {
+                    this->data[x+k + y*this->width*this->channels] = 
+                        originalData[i+k + j*originalResource->GetWidth()*this->channels];
+                }
+            }
+        }
+
+        this->data = data;
+    }
+
+    void Unload(){
+        originalResource->Unload();
+        if (this->data) {
+            delete[] (T*) this->data;
+            this->data = NULL;
+        }
+    }
 
 };
 
